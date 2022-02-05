646f2fb7266ef6a724776b812f91f6bdfc5743c0
Aleksei Botka <alekseibytka@gmail.com>
2022-02-05 17:10:39 +0200

    Removed SECRET_KEY from settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
